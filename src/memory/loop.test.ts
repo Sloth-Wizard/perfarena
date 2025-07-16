e4f650f7b3538146663de91a549b17bc2967d1c2
Sloth-Wizard <35430441+Sloth-Wizard@users.noreply.github.com>
2025-07-16 08:52:08 +0200

    test forEach loop

--- src/memory/loop.test.ts
+++ src/memory/loop.test.ts
@@ -10,6 +10,14 @@ function forLoop(arr: Array<number>): number {
     return sum;
 }
 
+function forEach(arr: Array<number>): number {
+    let sum = 0;
+    arr.forEach((v) => {
+        sum += v;
+    });
+    return sum;
+}
+
 function reduce(arr: Array<number>): number {
     return arr.reduce((acc, x) => acc + x, 0);
 }
@@ -32,14 +40,15 @@ const GNNNNN = createArr(1_000_000);
 const runs = [1, 10, 100, 1000];
 
 let results = {
-    S:      {reduce: [] as Array<number>, forLoop: [] as Array<number>},
-    M:      {reduce: [] as Array<number>, forLoop: [] as Array<number>},
-    L:      {reduce: [] as Array<number>, forLoop: [] as Array<number>},
-    XL:     {reduce: [] as Array<number>, forLoop: [] as Array<number>},
-    GNNNNN: {reduce: [] as Array<number>, forLoop: [] as Array<number>},
+    S:      {reduce: [] as Array<number>, forLoop: [] as Array<number>, forEach: [] as Array<number>},
+    M:      {reduce: [] as Array<number>, forLoop: [] as Array<number>, forEach: [] as Array<number>},
+    L:      {reduce: [] as Array<number>, forLoop: [] as Array<number>, forEach: [] as Array<number>},
+    XL:     {reduce: [] as Array<number>, forLoop: [] as Array<number>, forEach: [] as Array<number>},
+    GNNNNN: {reduce: [] as Array<number>, forLoop: [] as Array<number>, forEach: [] as Array<number>},
 }
 
 // warm up
+time(100, S, forEach);
 time(100, S, reduce);
 time(100, S, forLoop);
 
@@ -51,14 +60,23 @@ for (let i = 0; i < runs.length; i++) {
     // Run it all
     results.S.reduce.push(time(run, S, reduce));
     results.S.forLoop.push(time(run, S, forLoop));
+    results.S.forEach.push(time(run, S, forEach));
+
     results.M.reduce.push(time(run, M, reduce));
     results.M.forLoop.push(time(run, M, forLoop));
+    results.M.forEach.push(time(run, M, forEach));
+
     results.L.reduce.push(time(run, L, reduce));
     results.L.forLoop.push(time(run, L, forLoop));
+    results.L.forEach.push(time(run, L, forEach));
+
     results.XL.reduce.push(time(run, XL, reduce));
     results.XL.forLoop.push(time(run, XL, forLoop));
+    results.XL.forEach.push(time(run, XL, forEach));
+
     results.GNNNNN.reduce.push(time(run, GNNNNN, reduce));
     results.GNNNNN.forLoop.push(time(run, GNNNNN, forLoop));
+    results.GNNNNN.forEach.push(time(run, GNNNNN, forEach));
 }
 
 console.log("RESULTS: ", results);
